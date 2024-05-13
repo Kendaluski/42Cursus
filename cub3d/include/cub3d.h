15c52486320b2a91f5346724369dea9ea9c2e5eb
Jose Mª Jaen Montesino <105867025+Kendaluski@users.noreply.github.com>
2024-05-13 13:48:43 +0200

    compilable

--- cub3d/include/cub3d.h
+++ cub3d/include/cub3d.h
@@ -6,16 +6,22 @@
 /*   By: jjaen-mo <jjaen-mo@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 12:54:14 by jjaen-mo          #+#    #+#             */
-/*   Updated: 2024/04/30 12:58:36 by jjaen-mo         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:46:03 by jjaen-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef CUB3D_H
+#ifndef CUB3D_H
 # define CUB3D_H
 
 	#include "libft/libft.h"
 	#include "MLX42/include/MLX42/MLX42.h"
+	#include <fcntl.h>
+	#include <math.h>
 
+	typedef struct s_data
+	{
+		mlx_t	*mlx;
 
+	}			t_data;
 
 #endif
--- cub3d/src/main.c
+++ cub3d/src/main.c
@@ -6,8 +6,34 @@
 /*   By: jjaen-mo <jjaen-mo@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 13:01:32 by jjaen-mo          #+#    #+#             */
-/*   Updated: 2024/04/30 13:01:33 by jjaen-mo         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:47:17 by jjaen-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/cub3d.h"
+#include "../include/cub3d.h"
+
+void ft_init_data(t_data *data)
+{
+	data->mlx = mlx_init(512,512,"Cub3D",false);
+}
+
+int main(int argc, char **argv)
+{
+	t_data *data;
+	
+	if(argc != 2)
+	{
+		printf("[ERROR] No map specified\n");
+		return (1);
+	}
+	if (argc == 2)
+	{
+		if (ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 4) != 0)
+		{
+			printf("[ERROR] Invalid file extension\n");
+			return (1);
+		}
+	}
+	data = malloc(sizeof(t_data));
+	ft_init_data(data);
+}
